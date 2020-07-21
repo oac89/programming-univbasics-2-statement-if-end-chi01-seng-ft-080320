c2a1f2c99ec241fb3f3601c98cac16928b5fdeea
Oscar A Castro <oacastro2@gmail.com>
2020-07-21 18:17:33 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,11 +1,5 @@
-chance_of_rain = -23
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter"
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-   puts "Pack an umbrella!"
-else
-  puts "Stay home and read Hegel."
-end
+puts "Hey, it's 2019!"
+
  
 
   
