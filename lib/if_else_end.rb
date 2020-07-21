eee60be7697c323cb20a165649d157474c3f346d
Oscar A Castro <oacastro2@gmail.com>
2020-07-21 18:36:04 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -3,3 +3,13 @@ if Time.now.to_i % 2 == 0
 else
   puts "Odd!"
 end  
+
+
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end  
